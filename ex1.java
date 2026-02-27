d80108b6e6e55479315f09f1901c2e30eec4a262
reuven.itzhakov <itzhakovreuven@gmail.com>
2026-02-27 14:22:43 +0200

    first commit

---
+++ ex1.java
@@ -0,0 +1,22 @@
+public class ex1 {
+    public static String invertCase(String s) {
+        if (s == null) return null;
+        StringBuilder sb = new StringBuilder(s.length());
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (Character.isLowerCase(c)) {
+                sb.append(Character.toUpperCase(c));
+            } else if (Character.isUpperCase(c)) {
+                sb.append(Character.toLowerCase(c));
+            } else {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        String input = "abc@@  ABC";
+        System.out.println(invertCase(input));
+    }
+}
